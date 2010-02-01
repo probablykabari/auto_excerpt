1804cc39950bbd015c80f0cf8dbc03deb718340c
Kabari Hendrick <manbehindtheman@kabari.name>
2010-01-31 19:57:31 -0600

    moving excerpt to a module so a string cna be returned

--- spec/auto_excerpt_spec.rb
+++ spec/auto_excerpt_spec.rb
@@ -8,23 +8,23 @@ describe AutoExcerpt do
    text = html_excerpt({:characters => 5, :ending => nil})
    stripped_text(text).length.should eql(5)
  
-   text = heavy_excerpt({:characters => 5, :ending => nil})
-   stripped_text(text).length.should eql(5)
+   text = heavy_excerpt({:characters => 7, :ending => nil})
+   stripped_text(text).length.should eql(7)
   end
-
+  
   it "should default to 150 characters" do
    text = html_excerpt(:ending => nil)
-   stripped_text(text).length.should eql(150)
+   stripped_text(text).length.should be_close(150, 7)
   end
 
-  it "does not include html tags in character count" do
+  it "does not include html tags or entities in character count" do
     AutoExcerpt.new("<h1>Hello World!</h1>", {:characters => 5, :ending => nil}).should == "<h1>Hello</h1>"
+    AutoExcerpt.new("<h1>Copyright &copy; 2010</h1>", {:characters => 11, :ending => nil}).should == "<h1>Copyright &copy;</h1>"
   end
   
   it "should not cutoff in the middle of a word" do
-    pending("this does not work yet") do
-      AutoExcerpt.new("<h1>Hello World!</h1>", {:characters => 4, :ending => nil}).should == "<h1>Hello</h1>"
-    end
+    AutoExcerpt.new("<h1>Hello World!</h1>", {:characters => 4, :ending => nil}).should == "<h1>Hello</h1>"
+    AutoExcerpt.new("<h1>Hello World!</h1>", {:characters => 7, :ending => nil}).should == "<h1>Hello World</h1>"
   end
   
   it "should limit words" do
@@ -40,8 +40,8 @@ describe AutoExcerpt do
    text = html_excerpt({:sentences => 3})
    text.should == %{<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...</p>}
  
-   # text = heavy_excerpt({:sentences => 3})
-   # text.should == %{<p>Alright&hellip;ok&hellip;that title is a bold faced lie. I don&rsquo;t give a damn about <acronym title="Cascading Style Sheets">CSS</acronym> validation! Being a designer for a living, you have to know when to ditch some of these &lsquo;web 2.0&rsquo; type fads...</p>} 
+   text = heavy_excerpt({:sentences => 3})
+   text.should == %{<p>Alright&hellip;ok&hellip;that title is a bold faced lie. I don&rsquo;t give a damn about <acronym title="Cascading Style Sheets">CSS</acronym> validation. Being a designer for a living, you have to know when to ditch some of these &lsquo;web 2.0&rsquo; type fads...</p>} 
   end
 
   it "should limit paragraphs" do
